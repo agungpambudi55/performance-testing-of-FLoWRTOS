f5327263865a94f5f0ec631933b42b83fca21556
agungpambudi55 <agung.pambudi5595@gmail.com>
2018-07-12 14:20:52 +0700

    update

--- main.c
+++ main.c
@@ -16,10 +16,24 @@
 uint32_t time_start_task, time_finish_task;
 
 void task1(){	// -+ 44 us
-	time_start_task = count_time_us;
+	// time_start_task = count_time_us;
 	get_euler_angles();
-	time_finish_task = count_time_us;
-	PRINTF("\n\rIMU = %u us\n\r", (time_finish_task - time_start_task));
+	// time_finish_task = count_time_us;
+	// PRINTF("\n\rIMU = %u us\n\r", (time_finish_task - time_start_task));
+}
+
+void task2(){	// -+ 42 us
+	// time_start_task = count_time_us;
+	kinematic_control(0, 0, 0, true);
+	// time_finish_task = count_time_us;
+	// PRINTF("\n\rIK_C = %u us\n\r", (time_finish_task - time_start_task));
+}
+
+void task3(){	// -+ 5146 us
+	// time_start_task = count_time_us;
+	set_position();
+	// time_finish_task = count_time_us;
+	// PRINTF("\n\rDXL = %u us\n\r", (time_finish_task - time_start_task));
 }
 
 void main(void){
@@ -28,6 +42,8 @@ void main(void){
     management_timer_init();
     management_task_init();
     create_task("Task_1", task1, 1000);
+    create_task("Task_2", task2, 2000);
+    create_task("Task_3", task3, 7000);
     learning(1000000);
     scheduler();
     while(true);
